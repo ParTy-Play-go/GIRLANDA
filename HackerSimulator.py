42916c163fc26cc7327fb940d7257e4c5a7b72e8
ParTy-Play-go <52920235+ParTy-Play-go@users.noreply.github.com>
2020-03-20 18:30:08 +0300

    Add files via upload

---
+++ HackerSimulator.py
@@ -0,0 +1,42 @@
+import time
+print("Starting system...")
+time.sleep(5)
+print("Started!")
+print("Connecting to servers...")
+time.sleep(9)
+print("Connected!")
+cash = 500
+
+
+
+
+
+lan = input ("root@HackerSimulator:/$ Choose your language[RU, EN]: ")
+
+def starten() :
+	print("Hey! I'm Tiny!")
+	print("For help please send help")
+	
+def startru() :
+	print("Хей! Я Тини!")
+	print("Я помогу тебе в твоем развитии в мире хакеров!")
+	print("Для начала тура по игре напиши tour")
+	
+
+
+
+
+if (lan == "EN"):
+	starten()
+	com = input("root@HackerSimulator:/$: ") 
+
+if (lan == "RU", "ru", "Ru"):
+	startru()
+	comru = input("root@HackerSimulator:/$: ")
+elif (comru == "help"):
+	print("Помощь: ")
+	print("•help — вызвать это меню")
+	print("•tour — проити тур по игре")
+	
+	
+elif (comru == "tour")
